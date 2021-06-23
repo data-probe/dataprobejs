f64d9295ef334f3a818737964740f2a5033d05b6
dataprobe <86370225+data-probe@users.noreply.github.com>
2021-06-23 21:49:46 +0530

    first stable release

---
+++ README.md
@@ -0,0 +1,56 @@
+## About 
+
+This is a Javascript library for data exploration and analysis.
+It takes data input in JSON format and converts it into a tabular form
+in the node console. It has simple to use commands for manipulating and 
+playing around with the data table
+
+### Installation
+
+```javascript
+npm i dataprobe
+```
+### Important Functions
+
+* console.table(dataset) - Used to display the data in tabular format in your terminal/console
+
+* dataHead(dataset,number) - It takes two input, first the data and then the number of rows.
+This function is used to display a perticular number of rows from the top of the data table
+
+* dataTail(dataset,number) - Works same as dataHead(), instead displays a certain number of 
+rows from the bottom depending on input.
+
+* datasetSize(dataset) - Displays the size of the data table. 
+
+* dataSummary(dataset) - Provides a summary of the data table.
+
+* columnEncoder(dataset,column-name) -This function takes two inputs, first the data and
+then the column to be encoded.It converts multivariant data to numeric data for easies and faster 
+data processing
+
+* sliceColumn(dataset,column-name) - Used to slics a column
+
+* columnsCorrelation(column1-name,column2-name) - returns the correlation coefficient of two arrays. It takes two sliced arrays as input. Correlation coefficient are always on the range of 
+-1 to +1. 
+
+* checkNull(dataset,column-name) - Used to check and count the presence of null,undefined,"" or 0 values inside a given column of the data table. It return a number. If it returns 0, then the column has no input values
+
+* deleteColumn(dataset,column-name) - Delete a column form the database.
+
+* saveDataset(new_dataset,filename) - Takes two inputs, first the data and then the JSON filename of the where you want to store your new dataset in JSON format
+
+### How to Import dataprobe and use it ?
+
+```javascript
+const {functionName()} = require("dataprobe);
+const dataset = require("./filename");
+```
+#### Example
+```javascript
+const {datasetSize} = require("dataprobe);
+const dataset = require("./supermarket.json");
+
+console.table(dataset);
+datasetSize(dataset);
+```
+
---
+++ index.js
@@ -0,0 +1,127 @@
+const fs = require("fs");
+// Drop a Column from the Dataset
+const deleteColumn = (dataset, key) => {
+    for (let i = 0; i < dataset.length; i++) {
+        delete dataset[i][key];
+    }
+    return (dataset);
+}
+
+
+// Size of the Dataset
+const datasetSize = (dataset) => {
+    rows = Object.keys(dataset[0]).length;
+    columns = dataset.length;
+    return "Total rows: " + rows + " and " + "Total  columns: " + columns;
+}
+
+
+// Dataset Summary
+const dataSummary = (dataset) => {
+    console.log()
+    console.log("Data Summary :-")
+    console.log()
+    for (let i in dataset[0]) {
+        console.log(i + " : " + typeof (dataset[0][i]));
+    }
+    columns = Object.keys(dataset[0]).length;
+    rows = dataset.length;
+    console.log("Total rows: " + rows);
+    console.log("Total columns: " + columns);
+}
+
+
+// Data Transform
+const columnEncoder = (dataset,columns) =>{
+    var set = new Set(dataset.map(item => item[columns]))
+    const arr = [...set]
+    for (let i = 0;i<dataset.length;i++){
+        if(arr.includes(dataset[i][columns])){
+            let temp = dataset[i][columns]
+            dataset[i][columns] = arr.indexOf(temp)
+        }
+    }
+    return dataset;
+}
+
+// Column slicing
+const sliceColumn = (dataset, column) => {
+    const col = []
+    for (let i = 0; i < dataset.length; i++) {
+        col.push(dataset[i][column]);
+    }
+    return col;
+}
+
+
+
+// Correlation Coefficient
+const columnsCorrelation = (X, Y) => {
+    let n = X.length;
+    let sum_X = 0, sum_Y = 0, sum_XY = 0;
+    let squareSum_X = 0, squareSum_Y = 0;
+    for (let i = 0; i < n; i++) {
+        sum_X = sum_X + X[i];
+        sum_Y = sum_Y + Y[i];
+        sum_XY = sum_XY + X[i] * Y[i];
+        squareSum_X = squareSum_X + X[i] * X[i];
+        squareSum_Y = squareSum_Y + Y[i] * Y[i];
+    }
+    let corr = (n * sum_XY - sum_X * sum_Y) /
+        (Math.sqrt((n * squareSum_X -
+            sum_X * sum_X) *
+            (n * squareSum_Y -
+                sum_Y * sum_Y)));
+    return corr.toFixed(3);            
+}
+
+
+// Head and Tail 
+const dataHead = (dataset,num)=>{
+    const headDataset = []
+    for(let i =0;i<num;i++){
+        headDataset.push(dataset[i]);
+    }
+    return headDataset
+}
+
+const dataTail = (dataset,num)=>{
+    const rdataset = dataset.reverse()
+    const tailDataset = []
+    for(let i =0;i<num;i++){
+        tailDataset.push(dataset[i]);
+    }
+    return tailDataset.reverse()
+}
+
+// Checks and counts the total number of  null values of a column 
+const checkNull = (dataset,column)=>{
+    let counter = 0
+    for(let i=0;i<dataset.length;i++){
+        if(!dataset[i][column]){
+            counter++
+        }
+    }
+    return counter 
+}
+
+// Save dataset to new json file
+const saveDataset = (dataset,filename)=>{
+    fs.writeFile(filename,JSON.stringify(dataset),"utf8",(err)=>{
+        console.log(err);
+    });
+}
+
+
+module.exports = {
+    deleteColumn,
+    datasetSize,
+    dataSummary,
+    columnEncoder,
+    sliceColumn,
+    columnsCorrelation,
+    dataHead,
+    dataTail,
+    checkNull,
+    saveDataset
+}
